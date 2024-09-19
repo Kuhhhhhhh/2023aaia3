fb80e2fcb4ff2b498e0836f8f2e099ec0d4dfea1
kuhhhhhhh <touruuu@gmail.com>
2024-09-19 15:08:38 +0800

    week02

---
+++ week02/week02-2.cpp
@@ -0,0 +1,14 @@
+//02-2.cpp(C++)
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	for(char c : s){
+		if(c != '2')cout << c;
+	}
+	cout <<"\n";
+}
