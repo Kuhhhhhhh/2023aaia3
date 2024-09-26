ec9c30f7e2fd87463941ea73d9f774668adb514f
kuhhhhhhh <touruuu@gmail.com>
2024-09-26 15:27:38 +0800

    week03

---
+++ week03/week03-4.cpp
@@ -0,0 +1,8 @@
+//week03-4.cpp 學習計畫第3題，大海撈針 在haystack裡找needle
+//week03-4.cpp leetcode 28. Find the Index of the First Occurrence in a String
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);
+    }
+};
