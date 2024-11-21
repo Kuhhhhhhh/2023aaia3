74ef7bc4acc88dfc2b68a43d8796ae14d6b0df3c
kuhhhhhhh <touruuu@gmail.com>
2024-11-21 14:32:00 +0800

    week11

---
+++ week11/week11-1a.cpp
@@ -0,0 +1,11 @@
+//week11-1a.cpp_step01
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	while(cin>>n){//step01 : input
+		if(n==0)break;
+		cout << n << endl;
+	}
+}
---
+++ week11/week11-1b.cpp
@@ -0,0 +1,23 @@
+//week11-1b.cpp_step02
+#include <iostream>
+using namespace std;
+
+int f(int n){
+	int ans = 0;
+	while(n>0){
+		ans += n%10;
+		n = n/10;
+	}
+	return ans;
+}
+
+int main()
+{
+	int n;
+	while(cin>>n){//step01 : input
+		if(n==0)break;
+
+		n = f(n);
+		cout << n << endl;
+	}
+}
